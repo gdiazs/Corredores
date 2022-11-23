e62191057258e05455902f477474c3c1a20bda2a
gdiazs <diazgbs@gmail.com>
2022-11-23 00:11:31 -0600

    Valida campo

--- Models/TiemposModelo.cs
+++ Models/TiemposModelo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,10 @@ namespace Corredores.Models
     {
         public int CorredorID { set; get; }
 
+        [Required]
         public int Minutos { set; get; }
 
+        [Required]
         public int Segundos { set; get; }
     }
 }
