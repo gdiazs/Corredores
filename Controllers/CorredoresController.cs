c2aa8bd722768e710b2f2f941d4c9e91a188a6f8
gdiazs <diazgbs@gmail.com>
2022-11-22 19:07:02 -0600

    Primera versión

---
+++ Controllers/CorredoresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Corredores.Controllers
+{
+    public class CorredoresController : Controller
+    {
+        // GET: Corredores
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
